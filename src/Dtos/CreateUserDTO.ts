d8aefd5ea34e268bc0b26eb6ca34d27dfb328b49
jhongomes <jhonatangomes859@gmail.com>
2021-05-26 12:00:30 -0400

    Adicionando CreateUserDTOS

---
+++ src/Dtos/CreateUserDTO.ts
@@ -0,0 +1,12 @@
+export default interface CreateUserDTO{
+  name: string;
+  lastname: string;
+  nickname: string;
+  email: string;
+  password: string;
+  cpf: string;
+  birth_date: string;
+  phone: string;
+  address: string;
+  bio: string;
+}
