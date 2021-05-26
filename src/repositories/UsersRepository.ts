2f27e9171b2311ae34f26b8b229f45d96e145f44
jhongomes <jhonatangomes859@gmail.com>
2021-05-26 13:58:01 -0400

    Implementando UserRepository

---
+++ src/repositories/UsersRepository.ts
@@ -0,0 +1,89 @@
+import IUsersRepository from "./IUsersRepository";
+import ICreateUsersDTO from "../Dtos/CreateUserDTO";
+import { User } from "../entities/User";
+import {  getRepository, Repository } from "typeorm";
+
+
+class UsersRepository implements IUsersRepository {
+
+  private ormRepository: Repository<User>;
+
+  constructor(){
+    this.ormRepository = getRepository(User);
+  }
+
+
+  public async create({
+      name,
+      lastname,
+      nickname,
+      email,
+      password,
+      cpf,
+      birth_date,
+      phone,
+      address,
+      bio
+  }: ICreateUsersDTO): Promise<User> {
+    const users = this.ormRepository.create({
+      name,
+      lastname,
+      nickname,
+      email,
+      password,
+      cpf,
+      birth_date,
+      phone,
+      address,
+      bio,
+
+    });
+
+      await this.ormRepository.save(users);
+
+
+      return users;
+  }
+
+
+  public async list(): Promise<User[]> {
+      return this.ormRepository.find();
+  }
+
+  public async save(users: User): Promise<User> {
+      return this.ormRepository.save(users);
+  }
+
+
+  public async remove(users: User): Promise<User> {
+       return this.ormRepository.remove(users);
+
+  }
+
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+       const user = await this.ormRepository.findOne({
+       where: { email }
+    })
+      return user;
+  }
+
+  public async findByNickname(nickname: string): Promise<User | undefined> {
+       return this.ormRepository.findOne({
+       where: { nickname }
+    })
+  }
+
+
+  public async findByCPF(cpf: string): Promise<User> {
+      const user = await this.ormRepository.findOne({
+       where: { cpf }
+    })
+
+      return user;
+
+  }
+
+}
+
+export { UsersRepository }
